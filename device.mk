6abe3f612f08314141017b4f8dcfc16d536031e5
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:09:12 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -60,3 +60,4 @@ PRODUCT_PACKAGES += \
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/xmsirius/xmsirius-vendor.mk)
 
+$(call inherit-product, vendor/lindroid/lindroid.mk)
